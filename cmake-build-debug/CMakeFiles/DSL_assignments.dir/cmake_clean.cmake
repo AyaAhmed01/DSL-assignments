6a5720dd113fe3a3a47c479cf5aadaab8d0cdb26
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-12-09 14:23:16 +0200

    Add merge sort based sol

---
+++ cmake-build-debug/CMakeFiles/DSL_assignments.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSL_assignments.dir/counting_inversions/dandq_inversion.cpp.obj"
+  "DSL_assignments.exe"
+  "DSL_assignments.exe.manifest"
+  "DSL_assignments.pdb"
+  "libDSL_assignments.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSL_assignments.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- counting inversions/dandq_inversion.cpp
+++ counting inversions/dandq_inversion.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int countSubInv(vector<int>::iterator st, vector<int>::iterator ed){
