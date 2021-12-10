8605f0a3798d8b6fa9467df1fda313c1957f6a0d
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-12-10 13:15:14 +0200

    Solve recursive top-down solution

--- dynamic programming/knapsack_with_repetition.cpp
+++ dynamic programming/knapsack_with_repetition.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // reference: https://www.youtube.com/watch?v=3zZcS43UKiA
 /* since there is no restrictions on whether the element is already taken or not (you could repeat), the only parameter
    that define a state is the current weight.
-   if answer of weight u is v(u), then v(u) = max(vi + v(u - wi)). The sub-problem is v(u - wi)
+   if answer of weight u is v(u), then v(u) = max(vi + v(u - wi)), for i [1, n]. The sub-problem is v(u - wi)
    O(n * W)
    */
 
@@ -23,6 +23,17 @@ int maxValueItr(vector<int> &weight, vector<int> &val, int totalCap){          /
     return dp[totalCap];
 }
 
+int maxValRec(vector<int> &dp, vector<int> &weight, vector<int> &val, int cap){
+    if(cap == 0)
+        return 0;
+    if(dp[cap] == -1)
+        dp[cap] = 0;
+        for(int i = 1; i < val.size(); i++){
+            if(cap >= weight[i])
+                dp[cap] = max(dp[cap], val[i] + maxValRec(dp, weight, val, cap - weight[i]));
+        }
+    return dp[cap];
+}
 
 int main(){
     int n, totalC;
@@ -32,7 +43,13 @@ int main(){
         cin>>w[i];
     for(int i = 1; i <= n; i++)
         cin>>v[i];
-    cout<<maxValueItr(w, v, totalC);
+    // iterative sol
+    cout<<maxValueItr(w, v, totalC)<<endl;
+
+    // recursion sol
+    vector<int> dp(totalC + 1, -1);
+    cout<<maxValRec(dp, w, v, totalC);
+
     return 0;
 }
 
