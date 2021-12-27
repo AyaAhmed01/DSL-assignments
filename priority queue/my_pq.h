4eaad2f2d998fabac3a8a5d71a3ca8d4b8d86864
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-12-27 13:07:00 +0200

    Create all public methods

--- priority queue/my_pq.h
+++ priority queue/my_pq.h
@@ -7,21 +7,46 @@ using namespace std;
 class My_PQ{
 private:
     vector<int> elements;
-    int maxSize;
+    int maxSize, curSize = 0;
+
+
 public:
     My_PQ(){}
 
     My_PQ(vector<int> arr, int maxSize = 1e6){}
 
-    int extract_max(){}
+    int extractMax(){
+        int result = elements[0];
+        elements[0] = elements[curSize - 1];
+        curSize--;
+        siftDown(0);
+    }
 
-    int change_priority(int index, int new_priority){}
+    int changePriority(int index, int newPriority){
+        auto oldP = elements[index];
+        elements[index] = newPriority;
+        if(oldP > newPriority)
+            siftDown(index);
+        else
+            siftUp(index);
+    }
 
-    int peek(){}
+    int peek(){
+        return elements[0];
+    }
 
-    void insert(int value){}
+    void insert(int value) throw (int){
+        if(curSize == maxSize)
+            throw maxSize;
+        elements[curSize] = value;
+        siftUp(curSize++);
+    }
 
-    void remove(int index){}
+    void remove(int index){  // change prior to -inf, then sift down
+        elements[index] = INT_MAX;
+        siftUp(index);
+        extractMax();
+    }
 };
 
 #endif
