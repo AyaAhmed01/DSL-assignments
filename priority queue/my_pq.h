a5bc246c3e6d10385b19499315a773413bf7f2df
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-12-27 13:07:44 +0200

    Implement siftUp and siftDown

--- priority queue/my_pq.h
+++ priority queue/my_pq.h
@@ -9,6 +9,41 @@ private:
     vector<int> elements;
     int maxSize, curSize = 0;
 
+    int leftChildIdx(int idx){
+        return idx * 2 + 1;
+    }
+
+    int rightChildIdx(int idx){
+        return idx * 2 + 2;
+    }
+
+    int parentIdx(int idx){
+        return floor((idx - 1)/ 2);
+    }
+
+    void siftUp(int idx){
+        while(idx > 0 && elements[parentIdx(idx)] < elements[idx]){
+            swap(elements[parentIdx(idx)], elements[idx]);
+            idx = parent(idx);
+        }
+    }
+
+    void siftDown(int idx){
+        int maxIdx = idx;
+        while(true){
+            int leftIdx = leftChildIdx(idx);
+            if(leftIdx < curSize && elements[leftIdx] > elements[maxIdx])
+                maxIdx = leftIdx;
+            int rightIdx = rightChildIdx(idx);
+            if(rightId < curSize && elements[rightIdx] > elements[maxIdx])
+                maxIdx = rightIdx;
+            if(i != maxIdx){
+                swap(elements[maxIdx], elements[idx]);
+                i = maxIdx;
+            } else
+                break;
+        }
+    }
 
 public:
     My_PQ(){}
